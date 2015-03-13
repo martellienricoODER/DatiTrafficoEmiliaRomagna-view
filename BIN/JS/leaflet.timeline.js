a15718a66e6c447e34ddb38cff26e27bb6528c5b
martellienricoODER <martellienrico@gmail.com>
2015-03-13 13:09:21 +0100

    bug

--- BIN/JS/leaflet.timeline.js
+++ BIN/JS/leaflet.timeline.js
@@ -370,7 +370,7 @@ http://leafletjs.com
       for (i = 0; i < leaflet_timeline_controls_to_remove.length; i++) {
         leaflet_timeline_controls_to_remove[i].parentNode.removeChild(leaflet_timeline_controls_to_remove[i]);
         }
-      this.timeline.setTime(this.start);  
+      this.timeline.setTime(this.end);  
 
       container = L.DomUtil.create('div', 'leaflet-control-layers ' + 'leaflet-control-layers-expanded ' + 'leaflet-timeline-controls ');
       if (this.timeline.options.enablePlayback) {
